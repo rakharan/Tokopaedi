1d6ea02d5bb9c48bb63d9b54b820573c686fa4e7
RakhaTF <rakha.tradersfamily@gmail.com>
2023-12-06 17:02:56 +0700

    feat: add Super Admin creation date.

--- src/migration/1701844827300-CreateSuperAdmin.ts
+++ src/migration/1701844827300-CreateSuperAdmin.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateSuperAdmin1701844827300 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const query = `INSERT INTO user (name, email, password, level) VALUES ('SuperAdmin', 'super.admin@gmail.com', '$2y$10$ppOiDmd9MsxJeNhoDZwuLuJ81/Tf7vD15ahZYcVN5009lAL02gMBG', 1)`
+        const query = `INSERT INTO user (name, email, password, level, created_at) VALUES ('SuperAdmin', 'super.admin@gmail.com', '$2y$10$ppOiDmd9MsxJeNhoDZwuLuJ81/Tf7vD15ahZYcVN5009lAL02gMBG', 1, 1701856885)`
         await queryRunner.query(query)
     }
 
