eb07057f14cfff51eb8b72f0078bded445c6c1f3
RakhaTF <rakha.tradersfamily@gmail.com>
2023-12-07 14:59:06 +0700

    fix: Table schema & migration fix.

--- src/migration/1701858387523-CreateUserManagementStaff.ts
+++ src/migration/1701858387523-CreateUserManagementStaff.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateUserManagementStaff1701858387523 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const query = `INSERT INTO user (name, email, password, level, created_at) VALUES ('User Management Staff', 'user.admin@gmail.com', '$2y$10$ppOiDmd9MsxJeNhoDZwuLuJ81/Tf7vD15ahZYcVN5009lAL02gMBG', 5, 1701856885)`
+        const query = `INSERT INTO user (name, email, password, level, created_at) VALUES ('User Management Staff', 'user.admin@gmail.com', '$2a$10$on3rrDVNqJjHQzsTOQgnaewQ0B7Mc4UVMuDF43KEmIdUsDXc16yEa', 5, 1701856885)`
         await queryRunner.query(query)
     }
 
--- src/migration/1701858403391-CreateShippingAndTransactionManagementStaff.ts
+++ src/migration/1701858403391-CreateShippingAndTransactionManagementStaff.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateShippingAndTransactionManagementStaff1701858403391 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const query = `INSERT INTO user (name, email, password, level, created_at) VALUES ('Shipping and Transaction Management Staff', 'SnT.admin@gmail.com', '$2y$10$ppOiDmd9MsxJeNhoDZwuLuJ81/Tf7vD15ahZYcVN5009lAL02gMBG', 6, 1701856885)`
+        const query = `INSERT INTO user (name, email, password, level, created_at) VALUES ('Shipping and Transaction Management Staff', 'SnT.admin@gmail.com', '$2a$10$on3rrDVNqJjHQzsTOQgnaewQ0B7Mc4UVMuDF43KEmIdUsDXc16yEa', 6, 1701856885)`
         await queryRunner.query(query)
     }
 
