e7144abaa84f413fce90b2c05e45286b8973c356
RakhaTF <rakha.tradersfamily@gmail.com>
2023-12-07 13:18:52 +0700

    fix: Add swagger schema.

--- src/adapters/inbound/http/routes/Admin.ts
+++ src/adapters/inbound/http/routes/Admin.ts
@@ -2,25 +2,43 @@ import { FastifyInstance, FastifyPluginOptions, RouteOptions } from "fastify";
 import ProductController from "@adapters/inbound/controller/ProductController";
 import { AuthValidate, CheckAuthAdmin } from "helpers/prehandler/AuthValidate";
 import { Rules } from "@domain/model/Rules";
+import * as Schema from "helpers/ApiSchema/ApiSchema"
 
 const routes: RouteOptions[] = [
-    {
-        method: ["POST"],
-        url: "/api/v1/product/delete",
-        preHandler: CheckAuthAdmin({ rules: Rules.DELETE_PRODUCT }),
-        handler: ProductController.DeleteProduct,
-    },
     {
         method: ["POST"],
         url: "/api/v1/product/create",
         preHandler: CheckAuthAdmin({ rules: Rules.CREATE_PRODUCT }),
         handler: ProductController.CreateProduct,
+        schema: {
+            body: Schema.BaseRequestSchema("Rakha", {
+                name: { type: "string" },
+                description: { type: "string" },
+                price: { type: "integer" },
+                stock: { type: "integer" },
+            }),
+            response: Schema.BaseResponse({ type: 'Boolean' })
+        }
+    },
+    {
+        method: ["POST"],
+        url: "/api/v1/product/delete",
+        preHandler: CheckAuthAdmin({ rules: Rules.DELETE_PRODUCT }),
+        handler: ProductController.DeleteProduct,
+        schema: {
+            body: Schema.BaseRequestSchema("Rakha", { id: { type: "integer" } }),
+            response: Schema.BaseResponse({ type: 'Boolean' })
+        }
     },
     {
         method: ["POST"],
         url: "/api/v1/product/update",
         preHandler: CheckAuthAdmin({ rules: Rules.UPDATE_PRODUCT }),
         handler: ProductController.UpdateProduct,
+        schema: {
+            body: Schema.BaseRequestSchema("Rakha", { id: { type: "integer" } }),
+            response: Schema.BaseResponse({ type: 'Boolean' })
+        }
     }
 ]
 
