92dff9065dc36927f62cfb58a732d7077e59710a
RakhaTF <rakha.tradersfamily@gmail.com>
2023-12-11 11:21:29 +0700

    feat: add rules for log & trashed table.
    Adding rules for managing history & deleted data table, not yet implemented.

--- src/migration/1701763953247-InsertSuperAdminRules.ts
+++ src/migration/1701763953247-InsertSuperAdminRules.ts
@@ -3,7 +3,36 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class InsertSuperAdminRules1701763953247 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const rules = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125];
+        const rules = [
+            100,
+            101,
+            102,
+            103,
+            104,
+            105,
+            106,
+            107,
+            108,
+            109,
+            110,
+            111,
+            112,
+            113,
+            114,
+            115,
+            116,
+            117,
+            118,
+            119,
+            120,
+            121,
+            122,
+            123,
+            124,
+            125,
+            126,
+            127,
+            128];
         const values = rules.map(rule => `(1, ${rule})`).join(', ');
         const query = `INSERT INTO user_group_rules (group_id, rules_id) VALUES ${values}`;
         await queryRunner.query(query);
