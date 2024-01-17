04ed6cd5fb775603d0a5c9983c9082c00aef7122
RakhaTF <rakha.tradersfamily@gmail.com>
2024-01-17 11:40:34 +0700

    feat: Add /health endpoint.
    - healthcheck endpoint, will return process uptime in second.

--- src/application/boot/fastify/base.ts
+++ src/application/boot/fastify/base.ts
@@ -4,9 +4,10 @@ import fastifyHelmet from "@fastify/helmet"
 import fastifyFormBody from "@fastify/formbody"
 import fastifyMulter from "fastify-multer"
 import { FilesObject } from "fastify-multer/lib/interfaces"
+import fastifyHealthCheck from 'fastify-healthcheck'
 
 export default fp(async (fastify: FastifyInstance) => {
-    await fastify.register(fastifyHelmet).register(fastifyFormBody).register(fastifyMulter.contentParser)
+    await fastify.register(fastifyHelmet).register(fastifyFormBody).register(fastifyMulter.contentParser).register(fastifyHealthCheck, {exposeUptime: true})
 })
 
 declare module 'fastify' {
