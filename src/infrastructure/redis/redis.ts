b57087d3447f712d69cfd269ca8fef1c1db67fcc
RakhaTF <rakha.tradersfamily@gmail.com>
2024-02-28 14:03:23 +0700

    fix: Add redis config.

--- src/infrastructure/redis/redis.ts
+++ src/infrastructure/redis/redis.ts
@@ -4,7 +4,12 @@ import path from "path"
 
 DotenvFlow.config({ path: path.resolve(__dirname, `../../../`) })
 
-export const redisClient = new Redis({ port: 6379 })
+const redisConfig = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+}
+
+export const redisClient = new Redis({ port: redisConfig.port, host: redisConfig.host })
 
 
 redisClient.on('error', (error: Error) => {
